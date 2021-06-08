51f3685ec5f769b2cd9eedfa4783a6250719f806
Frank Reding <frank.reding@gmail.com>
2021-06-08 21:00:34 +0200

    Refine

--- js/main.js
+++ js/main.js
@@ -5,23 +5,25 @@ const bottoms = [];
 const topEl = document.getElementById('top');
 const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
+const mainEl = document.querySelector('main');
 
 for (let i = 1; i <= max; i++) tops.push(`top-${i.toString().padStart(2, '0')}.jpg`);
 for (let i = 1; i <= max; i++) middles.push(`middle-${i.toString().padStart(2, '0')}.jpg`);
 for (let i = 1; i <= max; i++) bottoms.push(`bottom-${i.toString().padStart(2, '0')}.jpg`);
-
-console.log(tops, middles, bottoms);
 
 const summonDemon = () => {
 	const top = tops[Math.floor(Math.random() * max)];
 	const middle = middles[Math.floor(Math.random() * max)];
 	const bottom = bottoms[Math.floor(Math.random() * max)];
 	
-	console.log(top, middle, bottom);
-	
 	topEl.src = `img/${top}`;
 	middleEl.src = `img/${middle}`;
 	bottomEl.src = `img/${bottom}`;
+	
+	mainEl.classList.remove('shake');
+	setTimeout(() => {
+		mainEl.classList.add('shake');
+	}, 0);
 };
 
 document.getElementById('summon').addEventListener('click', summonDemon);
